5de5acc2485c50ab2fd1c8f54efe55bb93495c0e
Luiz Vicente Neto <luizvicenteneto777@gmail.com>
2026-04-14 17:50:35 -0300

    primeirocommit

--- main/caracteres.py
+++ main/caracteres.py
@@ -1,7 +1,8 @@
 import pandas as pd
-#import numpy as np
+import numpy as np
 
-X = pd.read_csv(r'C:\Users\Higor\Documents\5 sem\IA\Ep\Entradas\caracteres_modificado.txt')
-print(X)
-
-
+X = pd.read_csv(r'C:\Users\luizv\PyCharmMiscProject\EP_IA\Entradas\CARACTERES COMPLETO\X.txt', header=None)
+Y = pd.read_csv(r'C:\Users\luizv\PyCharmMiscProject\EP_IA\Entradas\CARACTERES COMPLETO\Y_letra.txt', header=None)
+print(X[120].value_counts())
+X[120]=Y[0]
+print(X[120].value_counts())
